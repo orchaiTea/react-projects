9f33a5aea0b49d63f084e5f761a59ea20648f6b0
Pushkar Tripathi <PushkarTripathi@proton.me>
2023-12-21 05:43:25 +0530

    store and state are configured.

--- 03blogWithBackend/src/App.jsx
+++ 03blogWithBackend/src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <>
-      <h1>Blog app</h1>
-    </>
-  );
+  const [loading, setLoding] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoding(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO:
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
