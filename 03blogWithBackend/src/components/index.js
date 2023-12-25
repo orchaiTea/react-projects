af2785ccd743026a059620681edd5fdc6829ec69
Pushkar Tripathi <PushkarTripathi@proton.me>
2023-12-25 08:24:47 +0530

    pages created.

--- 03blogWithBackend/src/components/index.js
+++ 03blogWithBackend/src/components/index.js
@@ -3,5 +3,21 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Post from "./Post";
+import PostForm from "./postForm/PostForm";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Post,
+  PostForm,
+  PostCard,
+};
